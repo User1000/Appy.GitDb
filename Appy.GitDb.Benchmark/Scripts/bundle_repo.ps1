c1380146be88b21659ce4f59d7b92796a46f7d73
stepanenko <stepanenko@soley.io>
2018-12-18 11:21:07 +0200

    exclude .net projects

--- Appy.GitDb.Benchmark/Scripts/bundle_repo.ps1
+++
@@ -1,8 +0,0 @@
-﻿param([string] $gitFolder,
-	  [string] $outputPath)
-
-$output = [System.IO.Path]::GetFullPath($outputPath)
-pushd $gitFolder
-git bundle create $output --branches --tags 2>&1
-popd
-Remove-Item -Recurse -Force $gitFolder
--- Appy.GitDb.Benchmark/Scripts/extract_repo.ps1
+++
@@ -1,4 +0,0 @@
-﻿param([string]$bundleFile,
-	  [string]$targetDir)
-
-git clone --bare $bundleFile $targetDir 2>&1 | write-host
